f21b14b33664c566a7f64d89fde119149e93a867
April Bollinger <63476704+aprilkat@users.noreply.github.com>
2021-12-12 16:04:16 -0600

    Added a Card Component and Axios

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,39 @@
 <template>
   <div>
     <Header/>
+    <Card/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from './components/Header.vue';
+import Card from './components/Card.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Card
+  },
+
+data(){
+  return{
+    shows: []
   }
-}
+},
+mounted(){
+  axios.get("https://api.themoviedb.org/3/tv/popular?api_key=1314a8e99666cc05df8ce544cd09f9f3&language=en-US&page=1").then ((response) => {
+    this.shows = response.data.results;
+  })
+
+}}
 </script>
 
 <style>
  * {
- margin: 0px;
- padding: 0px;
- text-align: center;
+  margin: 0px;
+  padding: 0px;
+  text-align: center;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="Card">
+        <img src="" alt="">
+        <h2>Title</h2>
+        <p>Overview</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Card"
+    }
+</script>
+
+<style scoped>
+    .Card{
+        
+        width: 30%;
+        padding: 5px;
+        margin: 10px;
+        box-shadow: 10px 10px 5px grey;
+
+    }
+</style>
