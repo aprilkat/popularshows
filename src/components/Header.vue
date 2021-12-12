df4698674e97125de97483914db6254483525ed3
April Bollinger <63476704+aprilkat@users.noreply.github.com>
2021-12-12 15:43:01 -0600

    Added a Header Component

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="Header">
+        <h1>Vue Popular Shows</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    .Header{
+        background-color: darkolivegreen;
+        color: white;
+        padding: 15px;
+    }
+</style>
